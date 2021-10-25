26668cd7f51b2ee669d2ae088d80cb2a07db4bec
Sean <sfduggan7@gmail.com>
2021-10-25 16:35:48 +0100

    Addition of DBCOnnection and Courses

---
+++ Models/CAOSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAOSelect.Models
+{
+    public class CAOSubject
+    {
+        public int CourseID { get; set; }
+        public String CourseName { get; set; }
+        public String Description { get; set; }
+        public int KeyWordCount { get; set; }
+    }
+}
