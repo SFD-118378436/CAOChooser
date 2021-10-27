a037bcd2e8854f8ba86599099db84ab400bbfc53
Sean <sfduggan7@gmail.com>
2021-10-27 11:22:37 +0100

    Updating to Azure based database

--- Helpers/DbHelper.cs
+++ Helpers/DbHelper.cs
@@ -10,7 +10,7 @@ namespace CAOSelect.Helpers
     {
         public static SqlConnection GetConnection() 
         {
-            return new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = CAOCheck;");
+            return new SqlConnection(@"Data Source=fypdatacao.database.windows.net;Initial Catalog=CAOCHECK;User ID=sduggan;Password=Knockboy@2;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
